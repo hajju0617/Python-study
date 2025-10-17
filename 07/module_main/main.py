af9ad913ce48814dc0ca6f96805df208f8e50bf6
hajju0617 <joowon023617@gmail.com>
2025-10-17 23:03:26 +0900

    Python - 7-3 복습.

---
+++ 07/module_main/main.py
@@ -0,0 +1,5 @@
+import test_module
+
+print('main의 __name__ 출력')
+print('main -> __name__ = ', __name__)
+print('----------------------')
---
+++ 07/module_main/test_module.py
@@ -0,0 +1,3 @@
+print('모듈의 __name__ 출력')
+print('test_module -> __name__ = ', __name__)
+print('----------------------')
